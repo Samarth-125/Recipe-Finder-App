28010b3a0d85f3f92fbcac751f9ba72d8202f142
Samarth <samarthnagpal125@gmail.com>
2026-04-01 19:01:32 +0530

    Added Sort, Search

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-const container = document.getElementById("container");
-const loading = document.getElementById("loading");
-
-const fetchMeals = async () => {
-    try {
-        const res = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a");
-        const data = await res.json();
-
-    loading.style.display = "none";
-    const meals = data.meals || [];
-    meals.forEach((meal) => {
-        const div = document.createElement("div");
-        div.className = "card";
-
-    div.innerHTML = `
-        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+const app=document.getElementById("app")
+const search=document.getElementById("search")
+const sort=document.getElementById("sort")
+let allMeals=[]
+const display=(meals)=>{
+    app.innerHTML=meals.map(meal=>`
         <h3>${meal.strMeal}</h3>
-        <p>${meal.strArea}</p>
-    `;
-
-    container.appendChild(div);
-    });
-
-} catch (error) {
-    loading.innerText = "Error loading data";
+        <img src="${meal.strMealThumb}" width="200"/>
+    `).join("")
 }
-};
 
-fetchMeals();
+async function getData(){
+    try{
+        const res=await fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+        const data=await res.json()
+        allMeals=data.meals||[]
+        display(allMeals)
+    }
+    catch(error){
+        app.innerText="Error"
+    }
+}
+
+search.addEventListener("input",()=>{
+    let filtered=allMeals.filter(meal=>
+        meal.strMeal.toLowerCase().includes(search.value.toLowerCase())
+    )
+
+    if(sort.value==="az"){
+        filtered.sort((a,b)=>a.strMeal.localeCompare(b.strMeal))
+    }
+    else if(sort.value==="za"){
+        filtered.sort((a,b)=>b.strMeal.localeCompare(a.strMeal))
+    }
+
+    display(filtered)
+})
+
+sort.addEventListener("change",()=>{
+    let filtered=[...allMeals]
+
+    if(search.value){
+        filtered=filtered.filter(meal=>
+            meal.strMeal.toLowerCase().includes(search.value.toLowerCase())
+        )
+    }
+
+    if(sort.value==="az"){
+        filtered.sort((a,b)=>a.strMeal.localeCompare(b.strMeal))
+    }
+    else if(sort.value==="za"){
+        filtered.sort((a,b)=>b.strMeal.localeCompare(a.strMeal))
+    }
+
+    display(filtered)
+})
+
+getData()
